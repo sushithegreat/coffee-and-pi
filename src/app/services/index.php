2d7da03d8b743e7a5baedd6e3a7e1ae4f869a8b3
sushithegreat <sushi.avenger@gmail.com>
2013-09-12 17:03:39 -0500

    adding slim to index for REST calls

--- src/app/services/index.php
+++ src/app/services/index.php
@@ -1,3 +1,27 @@
 <?php
 
-echo "howdy";
+require_once(dirname(dirname(dirname(__FILE__))).'/libraries/vendor/autoload.php');
+
+use PhpGpio\Gpio;
+
+//power pin constant
+define("POWERPIN", 23);
+
+$app = new \Slim\Slim();
+$app->get('/power', function () {
+    echo "Setting up power pin\n";
+	$gpio = new GPIO();
+	$gpio->setup(POWERPIN, "out");
+
+	echo "Turning on power pin\n";
+	$gpio->output(POWERPIN, 1);
+
+	echo "Sleeping!\n";
+	sleep(3);
+
+	echo "Turning off power pin\n";
+	$gpio->output(POWERPIN, 0);
+
+	echo "Unexporting all pins\n";
+	$gpio->unexportAll();
+});
