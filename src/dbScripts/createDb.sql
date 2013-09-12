9e1e31bde56fb97bae71a60a80ff3a4e6c12b5ec
sushithegreat <sushi.avenger@gmail.com>
2013-09-12 14:48:19 -0500

    adding DB creation script

---
+++ src/dbScripts/createDb.sql
@@ -0,0 +1,18 @@
+drop database coffeeAndPi;
+create database coffeeAndPi;
+
+use coffeeAndPi;
+
+create table alarms (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	time TIME  NOT NULL,
+	day ENUM('Sun', 'Mon' 'Tue', 'Wed', 'Thu', 'Fri', 'Sat')
+	);
+
+create table tasks (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	alarmId INT NOT NULL,
+	lastSnooze TIMESTAMP,
+	FOREIGN KEY (alarmId) REFERENCES alarms(id) ON DELETE CASCADE
+	);
+	
