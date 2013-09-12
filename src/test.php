16330636fdbb05cd305737da79ff067d089ad547
sushithegreat <sushi.avenger@gmail.com>
2013-09-12 12:18:10 -0500

    updated phpgpio library path

---
+++ src/test.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use PhpGpio\Gpio;
+
+//power pin constant
+define("POWERPIN", 23);
+
+echo "Setting up power pin\n";
+$gpio = new GPIO();
+$gpio->setup(POWERPIN, "out");
+
+echo "Turning on power pin\n";
+$gpio->output(POWERPIN, 1);
+
+echo "Sleeping!\n";
+sleep(3);
+
+echo "Turning off power pin\n";
+$gpio->output(POWERPIN, 0);
+
+echo "Unexporting all pins\n";
+$gpio->unexportAll();
