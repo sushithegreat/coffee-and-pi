9360e57b89b9120c49bc627b947a5a4b2705290a
sven7777 <sven7777@gmail.com>
2013-09-15 10:03:44 -0500

    updating php and adding new ui components

--- src/app/js/services.js
+++ src/app/js/services.js
@@ -1,13 +1,6 @@
 'use strict';
 
 /* Services */
-// angular.module('coffeeAndPiServices', ['ngResource']).
-//     factory('AlarmService', function($resource){
-// 		return $resource('services/:alarmId.json', {}, {
-//     		query: {method:'GET', params:{alarmId:'alarms'}, isArray: true}
-// 		}
-//   	);
-// });
 angular.module('coffeeAndPiServices', ['ngResource']).
 	factory('AlarmService', function($resource) {
 		return $resource('services/index.php/alarms/:id', {}, {
@@ -18,14 +11,3 @@ angular.module('coffeeAndPiServices', ['ngResource']).
 		}
 	);
 });
-
-// angular.module('coffeeAndPiServices', ['ngResource']).
-//     factory('AlarmService', function($resource){
-// 		return $resource('services/index.php/alarms/:alarmId', {}, {
-// 			query:  {method:'GET', params:{alarmId:''}, isArray:true},
-// 			post:   {method:'POST'},
-// 			update: {method:'PUT'},
-// 			remove: {method:'DELETE'}
-// 		}
-//   	);
-// });
